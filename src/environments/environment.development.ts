8ae635e9d26fb1704aa414c60cb2f50bbd51c3e8
Eng-Ahmed-Elsayed <55101484+Eng-Ahmed-Elsayed@users.noreply.github.com>
2023-08-29 15:25:29 +0300

    Create environment

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  baseApiUrl: 'https://localhost:7021/api/',
+  clientURI: 'http://localhost:4200/',
+  googleClientId:
+    '1098356996673-jub0mrmkh9m2askkirglaeccp75fjbep.apps.googleusercontent.com',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  baseApiUrl: 'https://localhost:7191/api/',
+  clientURI: 'http://localhost:4200/',
+  googleClientId:
+    '1098356996673-jub0mrmkh9m2askkirglaeccp75fjbep.apps.googleusercontent.com',
+};
+
+//clientSecret: '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'
