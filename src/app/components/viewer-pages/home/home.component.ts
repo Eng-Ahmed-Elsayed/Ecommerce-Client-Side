4234a05b41786d53146afb48bcf288f1c4e6bdc8
Eng-Ahmed-Elsayed <55101484+Eng-Ahmed-Elsayed@users.noreply.github.com>
2023-09-01 18:08:04 +0300

    update home component and mat-theme

--- src/app/components/viewer-pages/home/home.component.ts
+++ src/app/components/viewer-pages/home/home.component.ts
@@ -3,8 +3,38 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  // isAnimated = true;
+  itemsPerSlide = 5;
+  singleSlideOffset = true;
+  showIndicators = false;
 
+  slides = [
+    {
+      image: '../../../../assets/images/categories/c-1.avif',
+      name: 'PCs & Accessories',
+    },
+    {
+      image: '../../../../assets/images/categories/c-2.avif',
+      name: 'Mobile Phones',
+    },
+    {
+      image: '../../../../assets/images/categories/c-3.avif',
+      name: 'Televisions',
+    },
+    {
+      image: '../../../../assets/images/categories/c-1.avif',
+      name: 'PCs & Accessories',
+    },
+    {
+      image: '../../../../assets/images/categories/c-3.avif',
+      name: 'Televisions',
+    },
+    {
+      image: '../../../../assets/images/categories/c-2.avif',
+      name: 'Mobile Phones',
+    },
+  ];
 }
